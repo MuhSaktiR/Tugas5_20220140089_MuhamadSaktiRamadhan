93497096a7a45d15ab23035568ebab754d1f212a
MuhSaktiR <seken876@gmail.com>
2024-04-06 15:58:18 +0700

    Praktikum5PDW

---
+++ jsprak5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Yahhh Kena RickRoll Deng - Video");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Yahhh Kena RickRoll Deng - Audio");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Yahhh Kena RickRoll Deng - Gambar");
+});
